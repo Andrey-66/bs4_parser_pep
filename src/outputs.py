cc1bc7cae6cb67f38c11e5ad12f941a1bab74c76
Andrey-66 <andrey66-66-66@yandex.ru>
2023-12-16 20:17:09 +0300

    Update outputs.py

--- src/outputs.py
+++ src/outputs.py
@@ -3,17 +3,12 @@ import datetime as dt
 import logging
 
 from prettytable import PrettyTable
-from constants import BASE_DIR, DATETIME_FORMAT
+from constants import BASE_DIR, DATETIME_FORMAT, RESULTS_DIR
 
-
-def control_output(results, cli_args):
-    output = cli_args.output
-    if output == 'pretty':
-        pretty_output(results)
-    elif output == 'file':
-        file_output(results, cli_args)
-    else:
-        default_output(results)
+SAVE_FILE_MESSAGE = 'Файл с результатами был сохранён: {file_path}'
+PRETTY_OUTPUT = 'pretty'
+FILE_OUTPUT = 'file'
+FILE_ENCODING = 'utf-8'
 
 
 def default_output(results):
@@ -21,7 +16,7 @@ def default_output(results):
         print(*row)
 
 
-def pretty_output(results):
+def pretty_output(results, **kwargs):
     table = PrettyTable()
     table.field_names = results[0]
     table.align = 'l'
@@ -29,15 +24,30 @@ def pretty_output(results):
     print(table)
 
 
-def file_output(results, cli_args):
-    results_dir = BASE_DIR / 'results'
-    results_dir.mkdir(exist_ok=True)
+def file_output(results, cli_args, **kwargs):
+    directory = BASE_DIR / RESULTS_DIR
+    directory.mkdir(exist_ok=True)
     parser_mode = cli_args.mode
     now = dt.datetime.now()
     now_formatted = now.strftime(DATETIME_FORMAT)
     file_name = f'{parser_mode}_{now_formatted}.csv'
-    file_path = results_dir / file_name
-    with open(file_path, 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, dialect='unix')
+    file_path = directory / file_name
+    with open(file_path, 'w', encoding=FILE_ENCODING) as f:
+        writer = csv.writer(f, dialect=csv.unix_dialect)
         writer.writerows(results)
-    logging.info(f'Файл с результатами был сохранён: {file_path}')
+    logging.info(SAVE_FILE_MESSAGE.format(file_path=file_path))
+
+
+OUTPUT_FUNCIONS = {
+    PRETTY_OUTPUT: pretty_output,
+    FILE_OUTPUT: file_output,
+}
+
+
+def control_output(results, cli_args):
+    output = cli_args.output
+    if output in OUTPUT_FUNCIONS:
+        OUTPUT_FUNCIONS[output](results=results, cli_args=cli_args)
+    else:
+        default_output(results)
+
